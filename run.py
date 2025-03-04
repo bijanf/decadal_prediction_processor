0a8d88a826ba7eee33c6e29ba707565dd63bbbe9
Bijan Fallah <fallah@dkrz.de>
2025-03-04 12:49:15 +0100

    correct dates

--- run.py
+++ run.py
@@ -10,12 +10,12 @@ if __name__ == "__main__":
         variable="tas",
         ensemble="r26i2p1",
         climatology_file="/work/bk1318/k202208/diff-pred/data/mpi-esm/tas_Amon_MPI-ESM-historical_ensmean_185001-200512_climatology.nc",
-        output_file="/work/kd1418/codes/work/k202196/MYWORK/tas_Amon_MPI-ESM-LR_dkfen41979-2021_r26i2p1_f.nc", 
+        output_file=output_file, 
         output_dir="./", 
         cleanup=False 
     )
-    shift_initialization_time("/work/kd1418/codes/work/k202196/MYWORK/tas_Amon_MPI-ESM-LR_dkfen41979-2021_r26i2p1_f.nc",
-                          output_file)
+    #shift_initialization_time("/work/kd1418/codes/work/k202196/MYWORK/tas_Amon_MPI-ESM-LR_dkfen41979-2021_r26i2p1_f.nc",
+    #                      output_file)
 
     plot_global_mean_tas(
         output_file,
@@ -27,7 +27,4 @@ if __name__ == "__main__":
         num_lead_years=11,
         output_plot=f"first_11_lead_years_with_mean.png",
     )
-
-   
     
-
