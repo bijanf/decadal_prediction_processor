f395647f5321f8dd1a246728bd49113eaa3783f7
Bijan Fallah <fallah@dkrz.de>
2025-03-04 12:39:48 +0100

    test installation, add matplotlib, modify readme

--- run.py
+++ run.py
@@ -10,15 +10,15 @@ if __name__ == "__main__":
         variable="tas",
         ensemble="r26i2p1",
         climatology_file="/work/bk1318/k202208/diff-pred/data/mpi-esm/tas_Amon_MPI-ESM-historical_ensmean_185001-200512_climatology.nc",
-        output_file=output_file, 
+        output_file="/work/kd1418/codes/work/k202196/MYWORK/tas_Amon_MPI-ESM-LR_dkfen41979-2021_r26i2p1_f.nc", 
         output_dir="./", 
         cleanup=False 
     )
-    shift_initialization_time("/work/kd1418/codes/work/k202196/MYWORK/tas_Amon_MPI-ESM-LR_dkfen41979-2021_r26i2p1.nc",
-                          "/work/kd1418/codes/work/k202196/MYWORK/tas_Amon_shifted.nc")
+    shift_initialization_time("/work/kd1418/codes/work/k202196/MYWORK/tas_Amon_MPI-ESM-LR_dkfen41979-2021_r26i2p1_f.nc",
+                          output_file)
 
     plot_global_mean_tas(
-        "/work/kd1418/codes/work/k202196/MYWORK/tas_Amon_MPI-ESM-LR_dkfen41979-2021_r26i2p1.nc",
+        output_file,
         num_lead_years=2,
         output_plot=f"first_2_lead_years_with_mean.png",
     )
